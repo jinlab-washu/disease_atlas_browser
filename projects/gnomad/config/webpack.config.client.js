0feaa70d7042f56a90450c6f3cb0b1cccc66c9fd
Elle Mehinovic <130782571+LVMEHINOVIC@users.noreply.github.com>
2026-06-12 16:37:25 -0500

    Update webpack.config.client.js

--- projects/gnomad/config/webpack.config.client.js
+++ projects/gnomad/config/webpack.config.client.js
@@ -19,8 +19,17 @@ const config = {
   devServer: {
     historyApiFallback: true,
     port: 8008,
+    devMiddleware: {
     publicPath: '/',
-    stats: 'errors-only',
+    },
+    client: {
+    overlay: {
+      errors: true,
+      warnings: false,
+    },
+  },
+    //publicPath: '/',
+    //stats: 'errors-only',
   },
   devtool: 'source-map',
   entry: {
@@ -88,3 +97,94 @@ if (isDev) {
 }
 
 module.exports = config
+
+// const path = require('path')
+
+// const CopyWebpackPlugin = require('copy-webpack-plugin')
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const webpack = require('webpack')
+
+// const isDev = process.env.NODE_ENV === 'development'
+
+// const definitions = {
+//   'process.env.GNOMAD_API_URL': JSON.stringify(process.env.GNOMAD_API_URL),
+// }
+
+// const gaTrackingId = process.env.GA_TRACKING_ID
+// if (process.env.NODE_ENV === 'production' && !gaTrackingId) {
+//   console.log('\nWarning: No GA tracking ID for production build\n')
+// }
+
+// const config = {
+//   devServer: {
+//     historyApiFallback: true,
+//     port: 8008,
+//     publicPath: '/',
+//     stats: 'errors-only',
+//   },
+//   devtool: 'source-map',
+//   entry: {
+//     bundle: path.resolve(__dirname, '../src/client/index.js'),
+//   },
+//   mode: isDev ? 'development' : 'production',
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             rootMode: 'upward',
+//             plugins: [
+//                 "@babel/plugin-syntax-dynamic-import"
+//             ]
+//           },
+//         },
+//       },
+//       {
+//         test: /\.(gif|jpg|png)$/,
+//         use: {
+//           loader: 'file-loader',
+//           options: {
+//             outputPath: 'assets/images',
+//           },
+//         },
+//       },
+//     ],
+//   },
+//   output: {
+//     path: path.resolve(__dirname, '../dist/public'),
+//     publicPath: '/',
+//     filename: isDev ? '[name].js' : '[name]-[hash].js',
+//   },
+
+//   resolve: {
+//     alias: {
+//       process: 'process/browser',
+//     },
+//   },
+
+//   plugins: [
+//     new webpack.DefinePlugin(definitions),
+//     new webpack.ProvidePlugin({
+//       process: 'process/browser',
+//     }),
+//     new CopyWebpackPlugin({patterns: [{from: path.resolve(__dirname, '../src/client/opensearch.xml')}], 
+//     }),
+//     new HtmlWebpackPlugin({
+//       template: path.resolve(__dirname, '../src/client/index.html'),
+//       gaTrackingId: process.env.GA_TRACKING_ID,
+//     }),
+//   ],
+// }
+
+// if (isDev) {
+//   config.resolve = {
+//     alias: {
+//       'react-dom': '@hot-loader/react-dom',
+//     },
+//   }
+// }
+
+// module.exports = config
